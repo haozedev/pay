3025e87f84028d64c8f391de0d91d214a9672436
廖师兄 <null@163.com>
2019-11-08 17:05:01 +0800

    微信Native支付，发起支付成功

--- src/main/java/com/imooc/pay/service/impl/PayService.java
+++ src/main/java/com/imooc/pay/service/impl/PayService.java
@@ -2,14 +2,20 @@ package com.imooc.pay.service.impl;
 
 import com.imooc.pay.service.IPayService;
 import com.lly835.bestpay.config.WxPayConfig;
+import com.lly835.bestpay.enums.BestPayTypeEnum;
 import com.lly835.bestpay.model.PayRequest;
+import com.lly835.bestpay.model.PayResponse;
 import com.lly835.bestpay.service.impl.BestPayServiceImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 
 /**
  * Created by 廖师兄
  */
+@Slf4j
+@Service
 public class PayService implements IPayService {
 
 	/**
@@ -19,17 +25,25 @@ public class PayService implements IPayService {
 	 * @param amount
 	 */
 	@Override
-	public void create(String orderId, BigDecimal amount) {
+	public PayResponse create(String orderId, BigDecimal amount) {
 		WxPayConfig wxPayConfig = new WxPayConfig();
-		wxPayConfig.setAppId();
-		wxPayConfig.setMchId();
-		wxPayConfig.setMchKey();
+		wxPayConfig.setAppId("wxd898fcb01713c658");
+		wxPayConfig.setMchId("1483469312");
+		wxPayConfig.setMchKey("098F6BCD4621D373CADE4E832627B4F6");
+		wxPayConfig.setNotifyUrl("http://127.0.0.1");
 
 		BestPayServiceImpl bestPayService = new BestPayServiceImpl();
 		bestPayService.setWxPayConfig(wxPayConfig);
 
 		PayRequest request = new PayRequest();
+		request.setOrderName("4559066-最好的支付sdk");
+		request.setOrderId(orderId);
+		request.setOrderAmount(amount.doubleValue());
+		request.setPayTypeEnum(BestPayTypeEnum.WXPAY_NATIVE);
 
-		bestPayService.pay()
+		PayResponse response = bestPayService.pay(request);
+		log.info("response={}", response);
+
+		return response;
 	}
 }
---
+++ src/test/java/com/imooc/pay/service/impl/PayServiceTest.java
@@ -0,0 +1,23 @@
+package com.imooc.pay.service.impl;
+
+import com.imooc.pay.PayApplicationTests;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.math.BigDecimal;
+
+/**
+ * Created by 廖师兄
+ */
+public class PayServiceTest extends PayApplicationTests {
+
+	@Autowired
+	private PayService payService;
+
+	@Test
+	public void create() {
+		//BigDecimal.valueOf(0.01)
+		//new BigDecimal("0.01")  千万不要用 new BigDecimal(0.01)
+		payService.create("1234567891234567", BigDecimal.valueOf(0.01));
+	}
+}
